996d6212053156af861f6974d162c134ba5f3104
Abir Mahmud <amabirmahmud@gmail.com>
2020-10-08 01:20:12 +0600

    Update urls.py

--- My_Twitter_Clone/urls.py
+++ My_Twitter_Clone/urls.py
@@ -26,29 +26,27 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+     # Authentications
     # path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('accounts/login/', users_views.login_view, name='login'),
-
     # path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('accounts/logout/', users_views.logout_view, name='logout'),
 
+
+     # Password Reset by email
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
          name='password_reset'),
-
     path('password-reset/done',
          auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
-
     path('password-reset/confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
-
     path('password-reset/complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
          name='password_reset_complete'),
 
-
-    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+     #  Password Change
     path('password-change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
          name='password_change_done'),
@@ -56,14 +54,16 @@ urlpatterns = [
          name='password_change'),
 
 
-
+     # Signup User
     # path('register/', users_views.UserCreateView.as_view(), name='register_users'),
     path('register/', users_views.user_create_view, name='register_users'),
 
+     # Show Profile
     # path('profile/', users_views.profile, name='profile'),
 
     # path('search/', users_views.SearchView, name='search'),
 
+     # Go to Blog urls
     # path('', include('blog.urls')),
     path('', blog_views.index, name='home'),
 
